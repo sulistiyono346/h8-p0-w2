9809607c9b4b69dbf30046df49135aa3b0a72d59
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-07-04 12:28:48 +0700

    Tugas hari selasa

---
+++ exercises3.js
@@ -0,0 +1,32 @@
+var nama =`Sulistyo`;
+var peran=`Ksatria`;
+
+if (nama==""){
+  console.log("Nama harus diisi!");
+}
+else{
+  if(peran==""){
+    console.log("Halo,"+nama ,"Pilih peranmu untuk memulai game!");
+  }
+  else {
+    if(peran=="Ksatria"){
+      console.log("Selamat datang di Dunia Proxytia,"+nama);
+      console.log("Hallo Ksatria "+nama,",kamu dapat menyerang dengan senjatamu!!");
+    }
+    else{
+      if(peran=="Tabib"){
+        console.log("Selamat datang di Dunia Proxytia,"+nama);
+        console.log("Hallo Tabib "+nama ,",kamu akan membantu temanmu yang terluka.");
+      }
+      else {
+        if(peran=="Penyihir"){
+          console.log("Selamat datang di Dunia Proxytia,"+nama);
+          console.log("Hallo Penyihir "+nama,",ciptakan keajaiban yang membantu kemenanganmu!");
+        }
+        else {
+          console.log("Maff, "+nama,"Peran," +peran,"yang kamu pilih belum ada !!");
+        }
+      }
+    }
+  }
+}
