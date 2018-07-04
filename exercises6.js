64cbead449caaab411f4852d2b9bdcaff65212eb
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-07-04 19:03:46 +0700

    Tugas haris Rabu

---
+++ exercises6.js
@@ -0,0 +1,54 @@
+// // 1. Melakukan Looping Menggunakan While
+//
+// var counter = 2;
+// console.log("LOOPING PERTAMA");
+// while(counter <= 20) {
+//   console.log( counter +" - I love coding");
+//   counter +=2;
+// }
+//
+// var counter = 20;
+// console.log("LOOPING KEDUA");
+// while(counter >= 2) {
+//   console.log( counter +" - I will become fullstack developer");
+//   counter -=2;
+// }
+
+// //2. Melakukan Looping Menggunakan For
+// console.log("LOOPING PERTAMA");
+// for (var i = 1; i <=20 ; i++) {
+//   console.log( i +" - I love coding");
+// }
+// console.log("LOOPING KEDUA");
+// for (var i = 20; i >= 1 ; i--) {
+//   console.log( i +" - I love coding");
+// }
+// 3. Angka Ganjil dan Genap
+      //counter 1
+      // for (var i = 1; i <=100 ; i++) {
+      //     if ((i%2)=== 0){
+      //       console.log( i+". Genap");
+      //     }
+      //     else {
+      //       console.log( i+". Ganjil");
+      //     }
+      //   }
+      //counter 2
+        // for (var i = 1; i <=100 ; i+=2) {
+        //     if ((i%3)=== 0){
+        //       console.log( i+" Kelipatan 3");
+        //     }
+        //   }
+
+        // for (var i = 1; i <=100 ; i+=5) {
+        //     if ((i%6)=== 0){
+        //       console.log( i+" Kelipatan 6");
+        //     }
+        //   }
+
+        // for (var i = 1; i <=100 ; i+=9) {
+        //     if ((i%10)=== 0){
+        //       console.log( i+" Kelipatan 10");
+        //     }
+        //
+        //   }
