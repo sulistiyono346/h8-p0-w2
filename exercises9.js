8c3d7dd4a0e286a46aa23ca69320e9c5134a5747
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-07-05 14:02:15 +0700

    Tugas Hari Kamis

--- exercises9.js
+++ exercises9.js
@@ -1,6 +1,37 @@
 //1. Tugas 1 fungsi bernama shoutOut()
 
-  function shoutOut() {
-    console.log("Halo Function!");
-  }
-  shoutOut();
+  console.log("1. Tugas 1 fungsi bernama shoutOut()");
+
+    function shoutOut() {
+      console.log("Halo Function!");
+    }
+    shoutOut();
+
+//2. Tugas 2 fungsi bernama calculateMultiply()
+
+  console.log("2. Tugas 2 fungsi bernama calculateMultiply()");
+
+     function calculateMultiply(num1,num2){
+        return num1*num2
+      }
+      var num1 = 5;
+      var num2 = 6;
+      var hasilPerkalian = calculateMultiply(num1,num2);
+      console.log(hasilPerkalian);
+
+//3. Tugas 3 fungsi bernama processSentence()
+
+  console.log("3. Tugas 3 fungsi bernama processSentence()");
+
+    function processSentence(name,age,address,hobby){
+
+      return ("Nama saya "+name+",umur saya " +age+" tahun, alamat saya di "+address+", dan saya punya hobby yaitu "+hobby);
+    
+    }
+    var name = "Agus ";
+    var age =30;
+    var address = "Jln. Malioboro, Yogjakarta";
+    var hobby ="gaming";
+
+    var fullSentence = processSentence(name,age,address,hobby);
+    console.log(fullSentence);
